c1bceddb4687ce0859c591b5c3b9ae8ddc502160
Sonny To <son.c.to@gmail.com>
2013-07-05 01:44:27 -0400

    initialize scale

--- src/com/datayumyum/blueScale/BlueScaleActivity.java
+++ src/com/datayumyum/blueScale/BlueScaleActivity.java
@@ -30,39 +30,41 @@ public class BlueScaleActivity extends Activity {
         if (mBluetoothAdapter == null) {
             Log.i(TAG, "adapter null");
         } else {
-            Log.i(TAG, "adapter=" + mBluetoothAdapter);
-            Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
-            for (BluetoothDevice device : pairedDevices) {
-                String name = device.getName();
-                Log.i(TAG, "device=" + name);
-                if (name.equals("Elane BT5")) {
-                    Log.i(TAG, "device=" + name);
-                    scale = new Scale(device);
-                    scale.onDataAvailable(new DataListener() {
-                        @Override
-                        public void process(Object data) {
-                            byte[] byteArray = (byte[]) data;
-                            byte[] weight = new byte[4];
-                            for (int i = 4, j = 0; i <= 7; i++) {
-                                weight[j++] = byteArray[i];
-                            }
-
-                            ByteBuffer byteBuffer = ByteBuffer.allocate(byteArray.length);
-                            byteBuffer.put(weight);
-                            Log.i(TAG, "weight=" + byteBuffer.asIntBuffer().get());
-                        }
-                    });
-
-                    break;
-                }
+            if (scale == null) {
+                initScale(mBluetoothAdapter);
             }
-
             byte[] tare = {0x07, 0x00, 0x72};
             byte[] readContinuous = {0x07, 0x00, 0x01};
             byte[] autoOffTimer = {0x07, 0x00, 0x7E, 0x00, 0x00, 0x00, 0x00, 127};
             scale.sendCmd(readContinuous);
+        }
+    }
 
+    private void initScale(BluetoothAdapter bluetoothAdapter) {
+        Log.i(TAG, "adapter=" + bluetoothAdapter);
+        Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
+        for (BluetoothDevice device : pairedDevices) {
+            String name = device.getName();
+            Log.i(TAG, "device=" + name);
+            if (name.equals("Elane BT5")) {
+                Log.i(TAG, "device=" + name);
+                scale = new Scale(device);
+                scale.onDataAvailable(new DataListener() {
+                    @Override
+                    public void process(Object data) {
+                        byte[] byteArray = (byte[]) data;
+                        byte[] weight = new byte[4];
+                        for (int i = 4, j = 0; i <= 7; i++) {
+                            weight[j++] = byteArray[i];
+                        }
+                        for (int j = 0; j < weight.length; j++) {
+                            Log.i(TAG, " " + weight[j]);
+                        }
+                    }
+                });
 
+                break;
+            }
         }
     }
 }
