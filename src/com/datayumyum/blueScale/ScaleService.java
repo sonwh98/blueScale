1193f72970116f087dfabfaef1de85c275f11a01
Sonny To <son.c.to@gmail.com>
2013-07-06 11:24:20 -0400

    ScaleService

---
+++ src/com/datayumyum/blueScale/ScaleService.java
@@ -0,0 +1,17 @@
+package com.datayumyum.blueScale;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+/**
+ * User: Sonny To sonny.to@conmio.com/son.c.to@gmail.com
+ * Date: 7/6/13
+ * Time: 2:45 AM
+ */
+public class ScaleService extends Service {
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
